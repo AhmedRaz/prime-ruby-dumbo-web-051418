47982770a02afb3e0b24ab3acb7a248497808e7d
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 13:28:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,12 +5,14 @@ def prime?(number)
   elsif number <=3
     return true 
   elsif number % 2 == 0 || number % 3 == 0 
-    return false 
+    return false
+  end 
   num1 = 5
   num2 = 2 
   while num1 * num1 <= number
     if number % num1 == 0 
-      return false 
+      return false
+    end
     num1 += num2
     num2 = 6 - num2
   end
